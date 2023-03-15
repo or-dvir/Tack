9bac12137efc61b3fa10b0ee2120823b66d3696a
Or Dvir <or_dvir@hotmail.com>
2023-03-15 12:20:16 +0100

    changed status bar color

--- app/src/main/java/com/hotmail/or_dvir/tack/ui/theme/Theme.kt
+++ app/src/main/java/com/hotmail/or_dvir/tack/ui/theme/Theme.kt
@@ -5,26 +5,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+private val primaryVariant = Color(0xff0288d1)
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xff0287c3),
+    primaryVariant = primaryVariant,
+    secondary = Color(0xff5af158),
+    secondaryVariant = Color(0xff48C046)
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Color(0xff03a9f4),
+    primaryVariant = primaryVariant,
+    secondary = Color(0xff5af158),
+    secondaryVariant = Color(0xff48C046)
 )
 
 @Composable
@@ -33,6 +31,13 @@ fun TackTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable (
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    //todo when the app starts, it uses the color in the xml file, and only then switches to this
+    // how can i fix this?
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(color = primaryVariant)
     }
 
     MaterialTheme(
